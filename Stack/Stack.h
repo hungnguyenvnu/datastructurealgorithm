e90d74996f508dff03871a991b22b0837dd700fa
Hung Nguyen <nguyenvanhung.uet@gmail.com>
2021-09-10 22:07:36 +0700

    Update Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -2,59 +2,63 @@
 #infdef _STACK_H_
 #define _STACK_H_
 
+class Node 
+{
+private:
+	int nData;
+	Node* pNext;
+public:		
+		// Constructor
+	Node (int nData) 
+	{
+		this->nData = nData;
+		this->pNext = nullptr;
+	}
+		
+	int GetData() 
+	{
+		return nData;
+	}
+};
+
 class Stack 
 {
-  private static class Node {
-		private String element;
-		private Node next;
-		
-		// Constructor
-		public Node (String e, Node n) {
-			element = e;
-			next = n;
-		}
-		
-		public String getElement() {
-			return element;
-		}
-		
-		public Node getNext() {
-			return next;
-		}
-		
-		public void setNext(Node n) {
-			next = n;
-		}
+private:
+	Node* pHead = null;
+	Node* pTail = null;
+	int nSize = 0;
+public:
+	Stack() {}
+	
+	int Size() 
+	{
+		return nSize;
 	}
 	
-	private Node head = null;
-	private Node tail = null;
-	int size = 0;
-	public Stack() {}
-	
-	public int size() {
-		return size;
+	bool IsEmpty() 
+	{
+		return (nSize == 0);
 	}
 	
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	
-	public String first() {
-		if (isEmpty()) {
-			return null;
+	int First() {
+		if (IsEmpty()) 
+		{
+		    std::cout << "List is empty!\n";
+			return 0;
 		}
-		return head.getElement();
+		return pHead->GetData();
 	}
 	
-	public String last() {
-		if (isEmpty()) {
-			return null;
+	int Last() {
+		if (IsEmpty()) 
+		{
+			std::cout << "List is empty!\n";
+			return 0;
 		}
-		return tail.getElement();
+		return pTail->GetData();
 	}
 	
-	public void push(String e) {
+	void Push(int nData) {
 		Node newest = new Node(e, head);
 		if (isEmpty()) {
 			head = tail = newest;
@@ -67,7 +71,7 @@ class Stack
 		return;
 	}
 	
-	public void pop() {
+	void Pop() {
 		if (isEmpty()) {
 			return;
 		}
@@ -78,6 +82,13 @@ class Stack
 		}
 	}
 };
+
+int main()
+{
+    cout<<"Hello World";
+
+    return 0;
+}
 
 #endif // _STACK_H_
 
