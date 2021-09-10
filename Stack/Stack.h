fba356fc0a6f95cf3f1eb44c4057f4fd6170754c
Hung Nguyen <nguyenvanhung.uet@gmail.com>
2021-09-10 20:44:48 +0700

    Update Stack.h

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,6 +1,84 @@
 ```
 #infdef _STACK_H_
 #define _STACK_H_
+
+class Stack 
+{
+  private static class Node {
+		private String element;
+		private Node next;
+		
+		// Constructor
+		public Node (String e, Node n) {
+			element = e;
+			next = n;
+		}
+		
+		public String getElement() {
+			return element;
+		}
+		
+		public Node getNext() {
+			return next;
+		}
+		
+		public void setNext(Node n) {
+			next = n;
+		}
+	}
+	
+	private Node head = null;
+	private Node tail = null;
+	int size = 0;
+	public Stack() {}
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public String first() {
+		if (isEmpty()) {
+			return null;
+		}
+		return head.getElement();
+	}
+	
+	public String last() {
+		if (isEmpty()) {
+			return null;
+		}
+		return tail.getElement();
+	}
+	
+	public void push(String e) {
+		Node newest = new Node(e, head);
+		if (isEmpty()) {
+			head = tail = newest;
+		} else {
+			Node newNext = newest.getNext(); 
+			newNext = head;
+			head = newest;
+		}
+		size++;
+		return;
+	}
+	
+	public void pop() {
+		if (isEmpty()) {
+			return;
+		}
+		head = head.getNext();
+		size--;
+		if (size == 0) {
+			tail = null;
+		}
+	}
+};
+
 #endif // _STACK_H_
 
 ```
